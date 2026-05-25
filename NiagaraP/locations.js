490abb3401519697ca71e77de640346672373c84
pelaxix <pelaxix@gmail.com>
2026-05-25 11:56:12 -0400

    Add Niagara Parks locations data

---
+++ NiagaraP/locations.js
@@ -0,0 +1,163 @@
+const NIAGARA_LOCATIONS = [
+  {
+    name: "Falls Parking Lot A",
+    type: "parking",
+    address: "6635 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.078917,
+    lng: -79.077998
+  },
+  {
+    name: "Floral Showhouse Parking Lot B",
+    type: "parking",
+    address: "7145 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.074068,
+    lng: -79.077791
+  },
+  {
+    name: "Rapidsview Parking Lot C",
+    type: "parking",
+    address: "7651 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.060665,
+    lng: -79.052031
+  },
+  {
+    name: "Queen Victoria Place",
+    type: "parking",
+    address: "6345 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.085948,
+    lng: -79.075319
+  },
+  {
+    name: "Dufferin Islands",
+    type: "parking",
+    address: "7400 Portage Road, Niagara Falls, ON",
+    lat: 43.069192,
+    lng: -79.077498
+  },
+  {
+    name: "Kingsbridge Park",
+    type: "parking",
+    address: "7870 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.054884,
+    lng: -79.042653
+  },
+  {
+    name: "Rainbow Bridge",
+    type: "parking",
+    address: "Rainbow Bridge, Niagara Falls, ON",
+    lat: 43.090508,
+    lng: -79.067771
+  },
+  {
+    name: "White Water Walk",
+    type: "parking",
+    address: "4330 River Road, Niagara Falls, ON",
+    lat: 43.117688,
+    lng: -79.060367
+  },
+  {
+    name: "Whirlpool Aero Car",
+    type: "parking",
+    address: "3850 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.119108,
+    lng: -79.064727
+  },
+  {
+    name: "Whirlpool Parkette",
+    type: "parking",
+    address: "Niagara River Parkway near Whirlpool Road, Niagara Falls, ON",
+    lat: 43.122397,
+    lng: -79.066319
+  },
+  {
+    name: "Fisherman's Parking Lot",
+    type: "parking",
+    address: "Niagara River Parkway near Niagara Glen, Niagara Falls, ON",
+    lat: 43.127066,
+    lng: -79.067124
+  },
+  {
+    name: "Thompson Point and WildPlay Adventure Course Lot",
+    type: "parking",
+    address: "3500 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.129827,
+    lng: -79.068979
+  },
+  {
+    name: "Butterfly Conservatory and Botanical Gardens",
+    type: "parking",
+    address: "2565 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.146905,
+    lng: -79.054551
+  },
+  {
+    name: "Niagara Glen and Nature Centre",
+    type: "parking",
+    address: "3050 Niagara River Parkway, Niagara Falls, ON",
+    lat: 43.136853,
+    lng: -79.061695
+  },
+  {
+    name: "Queenston Heights Park",
+    type: "parking",
+    address: "14184 Niagara River Parkway, Queenston, ON",
+    lat: 43.160654,
+    lng: -79.052869
+  },
+  {
+    name: "Old Fort Erie",
+    type: "parking",
+    address: "350 Lakeshore Road, Fort Erie, ON",
+    lat: 42.893719,
+    lng: -78.923225
+  },
+  {
+    name: "McFarland House",
+    type: "parking",
+    address: "15927 Niagara River Parkway, Niagara-on-the-Lake, ON",
+    lat: 43.224859,
+    lng: -79.063558
+  },
+  {
+    name: "Queenston Heights Boat Launch",
+    type: "boat-launch",
+    address: "Queenston Heights, Niagara River Parkway, Queenston, ON",
+    lat: 43.162638,
+    lng: -79.050669
+  },
+  {
+    name: "Ussher's Creek Boat Launch",
+    type: "boat-launch",
+    address: "Ussher's Creek Road, Niagara-on-the-Lake, ON",
+    lat: 43.203501,
+    lng: -79.060965
+  },
+  {
+    name: "Netherby Road Boat Launch",
+    type: "boat-launch",
+    address: "Netherby Road, Fort Erie, ON",
+    lat: 42.953892,
+    lng: -78.942848
+  },
+  {
+    name: "Anger Avenue Boat Launch",
+    type: "boat-launch",
+    address: "Anger Avenue, Fort Erie, ON",
+    lat: 42.903681,
+    lng: -78.924356
+  },
+  {
+    name: "Nichol's Marine Boat Launch",
+    type: "boat-launch",
+    address: "Nichol's Marine, Fort Erie, ON",
+    lat: 42.902219,
+    lng: -78.917366
+  },
+  {
+    name: "Niagara Parks Marina",
+    type: "boat-launch",
+    address: "Niagara Parks Marina, Fort Erie, ON",
+    lat: 42.884105,
+    lng: -78.924782
+  }
+];
